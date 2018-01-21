4ba742fa8c43ccd5f217c7c23e4000524344b77f
Ken Hall <kenhallthe3rd@gmail.com>
2018-01-20 16:40:08 -0800

    First draft main page of app concept

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>Diaster Relief App Beta</title>
+
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href=mainStyle.css>
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+	<h1>Disaster Response</h1>
+	<div id="mainContainer">
+		<div id="tabBar">
+			<div class="tabButton">
+				<div class="tabTextOneLine">
+				Suvivor
+				</div>
+			</div>
+			<div class="tabButton">
+				<div class="tabTextTwoLine">
+				First Responder
+				</div>
+			</div>
+			<div class="tabButton">
+				<div class="tabTextOneLine">
+				Volunteer
+				</div>
+			</div>
+			<div class="tabButton">
+				<div class="tabTextTwoLine">
+				Recovery Coordinator
+				</div>
+			</div>
+		</div>
+		<div id="containerContents">
+			<div id="containerContentsText">
+				Please select the tab that best describes you.
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
+
