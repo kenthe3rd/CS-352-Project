9906b0065bce889ccae8b6ff5fded16ad90658f3
Ken Hall <kenhallthe3rd@gmail.com>
2018-01-25 15:31:41 -0800

    Surv Mode Content + Externalized JS
    Added two buttons to Survivor Mode page
    Externalized javascript for main tab buttons

---
+++ mainJavascript.js
@@ -0,0 +1,16 @@
+$("#survButton").click(function()
+{
+	window.location.href="./survPage.html";
+});
+$("#respButton").click(function()
+{
+	window.location.href="./respPage.html";
+});
+$("#voluntButton").click(function()
+{
+	window.location.href="./voluntPage.html";
+});
+$("#coordButton").click(function()
+{
+	window.location.href="./coordPage.html";
+});
